d3aecce46a5643bc9253aa5d4362e4de0a5f18e1
Isac <contatoisacn@gmail.com>
2026-08-17 09:41:52 -0300

    feat: codificando as funcionalidades do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Pedido from "./components/Pedido"
 
 const App = () => {
   return (
     <>
+    <Pedido/>
     </>
   )
 }
--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import {useState} from 'react'
+
+//Array de objetos contendo o estado inicial do cardápio
+const cardapio=[
+    {id:1, nome:"Combo-01", preco:25.00, disponivel:true, quant:0},
+    {id:2, nome:"Combo-02", preco:35.00, disponivel:true, quant:0},
+    {id:3, nome:"Combo-03", preco:45.00, disponivel:false, quant:0},
+    {id:4, nome:"Combo-04", preco:55.00, disponivel:true, quant:0}
+];
 
 const Pedido = () => {
+
+    //HOOK - useState - Manipula o estado da variável
+    //Estados para gerenciar a lista de items
+    const[items,setItems]=useState(cardapio);
+    const[status,setStatus]=useState("");
+    const[enviar,setEnviar]=useState(false);
+
+    //Valor fixo adicionado ao total quando tiver 
+    // itens no carrinho
+    const taxaEntrega=5.00;
+
+    //função que altera a quantidade de um pedido
+    const alterarQuantidade =(id,valor)=>{
+        //usa a função updater para garantir o valor mais recente do estado
+        setItems(prev=>
+            //MAP: percorre a lista ara criar um NOVO array sem modificar 
+            // o original (IMUTABILIDADE)
+            prev.map(item=>
+                //TERNÁRIO: verifica se o item da iteração atual é o que deve ser
+                // alterado
+                //SPREAD (...item) : copia as propriedades do item e atualiza apenas a quantidade
+                //mantendo o resto
+                //Math.max : Objeto que garante que a quantidade nunca seja menor que 0
+                // Item: Retorna o item intacto caso o id não corresponda
+                item.id===id ? {...item,quantidade:Math.max(0,item.quantidade + valor)}: item
+            )
+            )
+    }
+
   return (
     <>
+    
     </>
   )
 }
